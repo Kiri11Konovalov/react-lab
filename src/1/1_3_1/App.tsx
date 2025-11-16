22c3a31569db22e1440bc25dd80c23b4aaa70f77
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-16 23:24:27 +0500

    1_3_1

--- src/1/1_3_1/App.tsx
+++ src/1/1_3_1/App.tsx
@@ -1,17 +1,17 @@
-// 1_3_1 Convert some HTML to JSX
-/* 
-  Этот HTML был вставлен в компонент, но это не корректный JSX. Исправьте его.
-*/
-
 export default function Bio() {
     return (
-      <div class="intro">
-        <h1>Welcome to my website!</h1>
+      // JSX-выражения должны иметь один родительский элемент.
+      <div>
+        {/* Атрибут "class" в HTML становится "className" в JSX. */}
+        <div className="intro">
+          <h1>Welcome to my website!</h1>
+        </div>
+        <p className="summary">
+          You can find my thoughts here.
+          {/* В JSX теги вроде <br> должны быть самозакрывающимися. */}
+          <br /><br />
+          <b>And <i>pictures</i></b> of scientists!
+        </p>
       </div>
-      <p class="summary">
-        You can find my thoughts here.
-        <br><br>
-        <b>And <i>pictures</b></i> of scientists!
-      </p>
     );
   }
