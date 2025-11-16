a35fbc4acb004b6f6a1cbaffb2d4ec82f0a72912
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-16 23:12:29 +0500

    1_2_1

---
+++ src/1/1_2_1/Profile.tsx
@@ -0,0 +1,9 @@
+// Компонент Profile теперь находится в своем собственном файле.
+export default function Profile() {
+    return (
+      <img
+        src="AlanHart.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
