804594f412034209b9f7c16d3b3fd8b922273b12
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-17 00:30:17 +0500

    1_5_3

--- src/1/1_5_3/App.tsx
+++ src/1/1_5_3/App.tsx
@@ -1,29 +1,32 @@
-// 1_5_3 Passing JSX in a children prop
-/*
-  Извлеките компонент Card из приведенной ниже разметки и используйте параметр children для передачи ему различных JSX.
-*/
+import { ReactNode } from 'react';
+
+// Компонент Card теперь принимает дочерние элементы через prop children.
+function Card({ children, title }: { children: ReactNode, title: string }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
+  );
+}
 
 export default function Profile() {
   return (
     <div>
-      <div className="card">
-        <div className="card-content">
-          <h1>Photo</h1>
-          <img
-            className="avatar"
-            src="OKS67lhm.jpg"
-            alt="Aklilu Lemma"
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-content">
-          <h1>About</h1>
-          <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
-        </div>
-      </div>
+      <Card title="Photo">
+        <img
+          className="avatar"
+          src="OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card title="About">
+        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+      </Card>
     </div>
   );
 }
