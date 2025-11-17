f3898053275a889075015f52c0e2243cd9108457
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-18 01:23:32 +0500

    2_2_3

--- src/2/2_2_3/App.tsx
+++ src/2/2_2_3/App.tsx
@@ -1,17 +1,13 @@
-// 2_2_3 Fix a crash
-/*
-  Вот небольшая форма, которая должна позволить пользователю оставить отзыв. Когда отзыв отправлен, предполагается отобразить сообщение с благодарностью. Однако при этом происходит сбой с сообщением об ошибке: "Rendered lesser hooks than expected". Можете ли вы заметить ошибку и исправить ее?
-*/
-
 import { useState } from 'react';
 
 export default function FeedbackForm() {
     const [isSent, setIsSent] = useState(false);
+    // Перемещаем хук useState для message сюда, чтобы соблюдать правила хуков
+    const [message, setMessage] = useState('');
+
     if (isSent) {
         return <h1>Thank you!</h1>;
     } else {
-        // eslint-disable-next-line
-        const [message, setMessage] = useState('');
         return (
             <form
                 onSubmit={(e) => {
