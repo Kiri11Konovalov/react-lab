2b1d2f2345ca649f7effd2cf6227468b04840918
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-18 15:27:11 +0500

    2_5_2

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -3,7 +3,16 @@ import { Que } from "./App";
 export function getFinalState(baseState: number, queue: Que[]) {
     let finalState = baseState;
 
-    // TODO: do something with the queue...
+    // Обрабатываем каждый элемент очереди
+    for (const update of queue) {
+        if (typeof update === 'function') {
+            // Если элемент очереди - функция, применяем её к текущему состоянию
+            finalState = update(finalState);
+        } else {
+            // Если элемент очереди - число, устанавливаем его как новое состояние
+            finalState = update;
+        }
+    }
 
     return finalState;
 }
