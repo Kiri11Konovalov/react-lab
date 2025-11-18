cec682a3c00ce3f7bcff1ff700d12a1098eef10a
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-19 01:47:29 +0500

    3_5_4

--- src/3/3_5_4/App.tsx
+++ src/3/3_5_4/App.tsx
@@ -1,15 +1,8 @@
-// 3_5_4  Implement useReducer from scratch 
-/*
-  В предыдущих примерах вы импортировали хук useReducer из React. В этот раз вам предстоит реализовать хук useReducer самостоятельно! Вот заглушка для начала работы. Он не должен занимать более 10 строк кода.
-
-  Чтобы проверить свои изменения, попробуйте ввести текст в поле ввода или выбрать контакт.
-*/
-
-import { useReducer } from 'react';
+// Реализован useReducer из MyReact.ts вместо React
+import { useReducer } from './MyReact';
 import Chat from './Chat';
 import ContactList from './ContactList';
 import { initialState, messengerReducer } from './messengerReducer';
-
 export default function Messenger() {
   const [state, dispatch] = useReducer(messengerReducer, initialState);
   const message = state.messages[state.selectedId];
--- src/3/3_5_4/MyReact.ts
+++ src/3/3_5_4/MyReact.ts
@@ -3,10 +3,12 @@ import { useState } from 'react';
 export function useReducer<State, Action>(
     reducer: (state: State, action: Action) => State,
     initialState: State
-) {
+): [State, (action: Action) => void] {
   const [state, setState] = useState(initialState);
 
-  // ???
+  const dispatch = (action: Action) => {
+    setState(reducer(state, action));
+  };
 
   return [state, dispatch];
 }
