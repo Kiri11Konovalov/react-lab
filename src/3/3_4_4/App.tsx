8e236b63a10cbbf89ed05c02d7e1a8cdbceb0552
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-18 23:39:09 +0500

    3_4_4

--- src/3/3_4_4/App.tsx
+++ src/3/3_4_4/App.tsx
@@ -1,19 +1,19 @@
-// 3_4_4 Clear an image while it’s loading
-/*
-  Когда вы нажимаете кнопку "Далее", браузер начинает загрузку следующего изображения. Однако, поскольку оно отображается в том же теге img, по умолчанию вы будете видеть предыдущее изображение, пока не загрузится следующее. Это может быть нежелательно, если важно, чтобы текст всегда совпадал с изображением. Измените это так, чтобы при нажатии кнопки "Next" предыдущее изображение сразу же убиралось.
-*/
-
+// Изображение теперь очищается при нажатии на кнопку "Next", а затем загружается новое
 import { useState } from 'react';
-
 export default function Gallery() {
   const [index, setIndex] = useState(0);
+  const [currentImageSrc, setCurrentImageSrc] = useState(images[0].src);
   const hasNext = index < images.length - 1;
 
   function handleClick() {
     if (hasNext) {
       setIndex(index + 1);
+      setCurrentImageSrc(''); // Очищаем изображение
+      setTimeout(() => setCurrentImageSrc(images[index + 1].src), 0); // Устанавливаем новое изображение
     } else {
       setIndex(0);
+      setCurrentImageSrc(''); // Очищаем изображение
+      setTimeout(() => setCurrentImageSrc(images[0].src), 0); // Устанавливаем новое изображение
     }
   }
 
@@ -26,7 +26,7 @@ export default function Gallery() {
       <h3>
         Image {index + 1} of {images.length}
       </h3>
-      <img src={image.src} />
+      <img src={currentImageSrc} />
       <p>
         {image.place}
       </p>
