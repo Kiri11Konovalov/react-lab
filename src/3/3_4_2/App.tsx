6c205d6d4230d2e148a77cc868227c8e36e3e231
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-18 23:13:50 +0500

    3_4_2

--- src/3/3_4_2/App.tsx
+++ src/3/3_4_2/App.tsx
@@ -1,10 +1,3 @@
-// 3_4_2 Swap two form fields
-/*
-  Эта форма позволяет вводить имя и фамилию. В ней также есть флажок, контролирующий, какое поле будет первым. Если установить флажок, поле "Фамилия" появится перед полем "Имя".
-
-  Это почти работает, но есть ошибка. Если вы заполните поле "Имя" и установите флажок, текст останется в первом поле (теперь это "Фамилия"). Исправьте это так, чтобы при изменении порядка ввода текст также перемещался.
-*/
-
 import { useState } from 'react';
 
 export default function App() {
@@ -22,22 +15,23 @@ export default function App() {
   if (reverse) {
     return (
       <>
-        <Field label="Last name" /> 
-        <Field label="First name" />
+        <Field key="lastName" label="Last name" />
+        <Field key="firstName" label="First name" />
         {checkbox}
       </>
     );
   } else {
     return (
       <>
-        <Field label="First name" /> 
-        <Field label="Last name" />
+        <Field key="firstName" label="First name" />
+        <Field key="lastName" label="Last name" />
         {checkbox}
       </>
-    );    
+    );
   }
 }
 
+// Компонент Field теперь использует ключи для сохранения состояния при изменении порядка
 function Field({ label }: { label: string }) {
   const [text, setText] = useState('');
   return (
