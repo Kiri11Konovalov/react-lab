05997b29eea1b98d8a0af42fabd3120364f08d2b
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-19 18:06:29 +0500

    4_3_3

--- src/4/4_3_3/Counter.tsx
+++ src/4/4_3_3/Counter.tsx
@@ -1,3 +1,5 @@
+// Добавлено очищение интервала при размонтировании компонента
+
 import { useState, useEffect } from 'react';
 
 export default function Counter() {
@@ -8,7 +10,13 @@ export default function Counter() {
       setCount(c => c + 1);
     }
 
-    setInterval(onTick, 1000);
+    // Сохраняем идентификатор интервала для последующей очистки
+    const intervalId = setInterval(onTick, 100);
+    
+    // Очищаем интервал при размонтировании компонента
+    return () => {
+      clearInterval(intervalId);
+    };
   }, []);
 
   return <h1>{count}</h1>;
