c3f4130ad3eb9ec1d35afb17d5bb49509f33ca3f
KirillKonovalov <kirill2002konovalov@gmail.com>
2025-11-19 23:29:54 +0500

    4_8_3

--- src/4/4_8_3/useInterval.ts
+++ src/4/4_8_3/useInterval.ts
@@ -1,5 +1,13 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-export function useInterval(callback: () => void, delay: number) {
-    // Тут нужно реализовать хук
+// Пользовательский хук useInterval
+export function useInterval(onTick: () => void, delay: number | null) {
+ useEffect(() => {
+    if (delay === null) {
+      return; // Не устанавливаем интервал, если delay равен null
+    }
+    
+    const id = setInterval(onTick, delay);
+    return () => clearInterval(id);
+  }, [onTick, delay]);
 }
